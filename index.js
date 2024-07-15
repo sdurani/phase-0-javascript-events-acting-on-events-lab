901c4dcbc36e6f783adc1cf5307af00ffaec5d08
Sama Durani <duranisama@gmail.com>
2024-07-14 23:38:07 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    const gameWidth = window.innerWidth;
+
+    const dodgerWidth = 40;
+    const rightEdge = left + dodgerWidth;
+
+    
+    if (rightEdge < gameWidth) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
